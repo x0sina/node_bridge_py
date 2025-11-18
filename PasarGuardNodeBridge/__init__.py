603cc2344d606bf5ea93b9a7e2d49db01399b57f
Sinacket <bulletsina77@gmail.com>
2025-11-18 15:03:03 +0330

    bump version to 0.2.4

--- PasarGuardNodeBridge/__init__.py
+++ PasarGuardNodeBridge/__init__.py
@@ -12,10 +12,10 @@ Features:
 - Extensible with custom metadata via the `extra` argument
 
 Author: PasarGuard
-Version: 0.2.3
+Version: 0.2.4
 """
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 __author__ = "PasarGuard"
 
 
